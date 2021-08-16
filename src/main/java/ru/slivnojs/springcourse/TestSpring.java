9a333ea7e116b36f57c8f9fc2bd7601abfa8cde2
VitalijVictor <vitalijvictor@gmail.com>
2021-08-16 14:19:03 +0200

    Spring Framework. Урок 7: Bean scope (Область видимости бинов).

--- src/main/java/ru/slivnojs/springcourse/TestSpring.java
+++ src/main/java/ru/slivnojs/springcourse/TestSpring.java
@@ -8,9 +8,20 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",
+                MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",
+                MusicPlayer.class);
 
-        musicPlayer.playMusicList();
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(50);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
